f076b780e02fae3b058628488303fda134415826
Michael Barth <sakenda1207@gmail.com>
2021-09-13 21:44:33 +0200

    Login Page neu gestaltet

--- KeySecret.DesktopApp/Views/LoginView.xaml.cs
+++ KeySecret.DesktopApp/Views/LoginView.xaml.cs
@@ -1,28 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.ComponentModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KeySecret.DesktopApp.Views
 {
     /// <summary>
     /// Interaction logic for LoginView.xaml
     /// </summary>
-    public partial class LoginView : UserControl
+    public partial class LoginView : UserControl, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _loginString;
+        public string LoginString
+        {
+            get => _loginString;
+            set
+            {
+                _loginString = value;
+                OnPropertyChanged(nameof(LoginString));
+            }
+        }
+
         public LoginView()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+        protected void OnPropertyChanged(string propertyname) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            LoginString = "TestString";
         }
     }
 }
