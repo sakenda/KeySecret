78080d8e432dacc23ddd2ac2431200477c0d5bf1
Andreas Scheuer <69506130+ascheuer-code@users.noreply.github.com>
2021-09-14 20:15:23 +0200

    Test (#2)
    Co-authored-by: Michael Barth <sakenda1207@gmail.com>

--- KeySecret.DesktopApp/Views/LoginView.xaml.cs
+++ KeySecret.DesktopApp/Views/LoginView.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System.ComponentModel;
+
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
 using System.Windows.Controls;
 
 namespace KeySecret.DesktopApp.Views
@@ -8,6 +16,7 @@ namespace KeySecret.DesktopApp.Views
     /// </summary>
     public partial class LoginView : UserControl, INotifyPropertyChanged
     {
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private string _loginString;
@@ -24,6 +33,27 @@ namespace KeySecret.DesktopApp.Views
         public LoginView()
         {
             InitializeComponent();
+
+            UsernameBox.GotFocus += new RoutedEventHandler(UsernameBox_GotFocus);
+            PasswordBox.GotFocus += new RoutedEventHandler(PasswordBox_GotFocus);
+        }
+
+
+        private void UsernameBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            UsernameBox.Foreground = new SolidColorBrush(Colors.Black);
+            if (UsernameBox.Text is "Username")
+            {
+                UsernameBox.Text = null;
+            }
+
+        }
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Foreground = new SolidColorBrush(Colors.Black);
+            PasswordBox.Clear();
+
             this.DataContext = this;
         }
 
