9cb6743879ad5138cc68c12badfd41b53cb99a35
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-09-14 17:45:59 +0200

    Test

--- KeySecret.DesktopApp/Views/LoginView.xaml.cs
+++ KeySecret.DesktopApp/Views/LoginView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,30 @@ namespace KeySecret.DesktopApp.Views
     /// </summary>
     public partial class LoginView : UserControl
     {
+
         public LoginView()
         {
             InitializeComponent();
+            UsernameBox.GotFocus += new RoutedEventHandler(UsernameBox_GotFocus);
+            PasswordBox.GotFocus += new RoutedEventHandler(PasswordBox_GotFocus);
+        }
+
+
+        private void UsernameBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            UsernameBox.Foreground = new SolidColorBrush(Colors.Black);
+            if (UsernameBox.Text is "Username")
+            {
+                UsernameBox.Text = null;
+            }
+
+        }
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Foreground = new SolidColorBrush(Colors.Black);
+            PasswordBox.Clear();
+
         }
     }
 }
