2ed7260d9c60d7a96b1386850b812998acbdffe4
Michael Barth <sakenda1207@gmail.com>
2021-09-18 18:33:00 +0200

    - Liste hinzugefügt um Api zu testen - API Port korrigiert

--- KeySecret.DesktopApp/MainWindow.xaml.cs
+++ KeySecret.DesktopApp/MainWindow.xaml.cs
@@ -1,15 +1,23 @@
 ﻿using KeySecret.DesktopApp.Library.DataAccess;
 using KeySecret.DesktopApp.Library.Models;
 using System;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Input;
 
 namespace KeySecret.DesktopApp
 {
     public partial class MainWindow : Window
     {
         public static string _categorie { get; set; }
+
         private IAccountEndpoint _accountEndpoint;
+        private ObservableCollection<AccountModel> _accountsList;
+
+        public ObservableCollection<AccountModel> AccountsList
+        {
+            get { return _accountsList; }
+            set { _accountsList = value; }
+        }
 
         public MainWindow(IAccountEndpoint accountEndpoint)
         {
@@ -19,7 +27,7 @@ namespace KeySecret.DesktopApp
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e) => Environment.Exit(0);
-        
+
         private void add(object sender, RoutedEventArgs e)
         {
             AddDialog _dialogBox = new AddDialog();
@@ -29,20 +37,16 @@ namespace KeySecret.DesktopApp
 
         private void remove(object sender, RoutedEventArgs e)
         {
-
             if (Categorie_Area.SelectedItem == null || Categorie_Area.SelectedItem.Equals(Allgemein))
             {
                 return;
             }
             Categorie_Area.Items.Remove(Categorie_Area.SelectedItem);
-
-
-
         }
+
         private void Quit_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
-
     }
 }
