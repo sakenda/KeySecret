8184a082b50ac091f21850ab895d806b58080641
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-10-04 11:40:04 +0200

    New Entry Dialogwindow hinzugefügt

---
+++ KeySecret.DesktopApp/AddPw.xaml.cs
@@ -0,0 +1,61 @@
+﻿using KeySecret.DesktopApp.Library.Accounts.Models;
+using KeySecret.DesktopApp.Library.DataAccess;
+using KeySecret.DesktopApp.Library.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KeySecret.DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for AddPw.xaml
+    /// </summary>
+    public partial class AddPw : Window
+    {
+        private IEndpoint<AccountModel, UpdateAccountModel, InsertAccountModel> _accountEndpoint;
+        public AddPw(IEndpoint<AccountModel, UpdateAccountModel, InsertAccountModel> accountEndpoint)
+        {
+            InitializeComponent();
+            _accountEndpoint = accountEndpoint;
+        }
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordTextBlock.Visibility = Visibility.Hidden;
+        }
+
+        private void NameBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WebAdress_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void New_Entry_Click(object sender, RoutedEventArgs e)
+        {
+            InsertAccountModel model = new InsertAccountModel();
+
+            model.Name = NameBox.Text;
+            model.WebAdress = WebadressBox.Text;
+            model.Password = PasswordBox.Password;
+
+            await _accountEndpoint.InsertAsync(model);
+            await ((MainWindow)Application.Current.MainWindow).LoadAccountsAsync();
+        }
+    }
+}
