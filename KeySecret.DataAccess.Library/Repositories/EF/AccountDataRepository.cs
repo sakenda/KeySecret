9f82f860dc255342c81fe72cc4c9a593fcb6c89f
Michael Barth <sakenda1207@gmail.com>
2021-11-06 22:33:01 +0100

    Detach Category entfernt

--- KeySecret.DataAccess.Library/Repositories/EF/AccountDataRepository.cs
+++ KeySecret.DataAccess.Library/Repositories/EF/AccountDataRepository.cs
@@ -1,6 +1,8 @@
 ﻿using KeySecret.DataAccess.Library.Context;
 using KeySecret.DataAccess.Library.Models;
+
 using Microsoft.EntityFrameworkCore;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -50,7 +52,7 @@ namespace KeySecret.DataAccess.Library.Repositories
 
             await _context.Accounts.AddAsync(item);
 
-            _context.Entry(item.Category).State = EntityState.Detached;
+            //_context.Entry(item.Category).State = EntityState.Detached;
 
             var result = await _context.SaveChangesAsync();
             if (result == 0)
